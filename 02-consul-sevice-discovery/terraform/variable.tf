4044fe0ea1e02f9a3d916523b883c1a3adf78cc6
useradm <useradm@d9710003.com>
2025-07-04 13:45:22 +0200

    Update deprecated terraform resources

--- 02-consul-sevice-discovery/terraform/variable.tf
+++ 02-consul-sevice-discovery/terraform/variable.tf
@@ -1,6 +1,7 @@
 variable "instance_type" {
    type = string
    description = "EC2 Instance Type"
+   default = "t2.micro"
  }
 
  variable "region" {
@@ -12,14 +13,17 @@ variable "instance_type" {
 variable "ami" {
    type = string
    description = "The amazon machine image"
+   default = "ami-0735c191cf914754d"
  }
 
 variable "key_name" {
    type = string
    description = "Key Name"
+   default = "projectsdemo"
  }
 
 variable "ingress_cidr_block" {
    type = string
    description = "The ingress CIDR block"
+   default = "172.31.0.0/16"
  }
